aed632e28f3547c8af3426329b3b720148dfc302
João Fernando <joaoxfernando@outlook.com>
2025-03-16 22:53:09 -0300

    Update some configs and plugins for nvim

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,8 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "python"},
+      auto_install = true,
+      ensure_installed = {"lua", "python", "c" },
       highlight = { enable = true },
       indent = { enable = true },
     })
